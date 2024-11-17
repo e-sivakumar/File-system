1ffa283b44cad68728bada71d19adcd6f5f72d16
Sivakumar <sivakumar.elango@bytecompass.tech>
2024-11-17 13:09:15 +0530

    Swagger implemented

--- app.js
+++ app.js
@@ -1,6 +1,9 @@
 const express = require('express');
 const fileRouter = require('./Router/fileRouter');
 const { multerRouter } = require('./Router/multerRouter');
+const swaggerUi = require('swagger-ui-express');
+const swaggerDocument = require('./Utils/swagger.json')
+
 require("dotenv").config();
 
 const PORT = process.env.PORT;
@@ -8,6 +11,7 @@ const PORT = process.env.PORT;
 const app = express();
 app.use(express.json());
 
+app.use('/api-docs', swaggerUi.serve, swaggerUi.setup(swaggerDocument))
 app.use('/api/multer', multerRouter)
 app.use("/api/file", fileRouter)
 
